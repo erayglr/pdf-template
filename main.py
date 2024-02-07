c8efac4f1f5141c3c55ff6eb060a3c8955f7da57
Eray <erayguler6757@gmail.com>
2024-02-07 03:34:25 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from fpdf import FPDF
+import pandas as pd
+
+# orientation L is horizontal P is vertical
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+
+    pdf.set_font("Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
+             ln=1)
+    pdf.line(10,21,200,21)
+# ln 1 ise ondan sonra gelecek metni altına yazar 0 ise iç içe
+# recommends size = h
+# align yazının ne tarafa hizalanacağını gösterir
+
+pdf.output("output.pdf")
