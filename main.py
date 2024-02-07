1140d17ed73cc2941ecd40ede32a92135ce853d1
Eray <erayguler6757@gmail.com>
2024-02-07 17:24:13 +0300

    Add lines

--- main.py
+++ main.py
@@ -14,8 +14,12 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
              ln=1)
-    pdf.line(10, 21, 200, 21)
+
+    for i in range(20, 298, 10):
+        pdf.line(10, i, 200, i)
+
     # Set the footer
+    i = 31
 
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
@@ -29,6 +33,9 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        for i in range(20, 298, 10):
+            pdf.line(10, i, 200, i)
 # ln 1 ise ondan sonra gelecek metni altına yazar 0 ise iç içe
 # recommends size = h
 # align yazının ne tarafa hizalanacağını gösterir
